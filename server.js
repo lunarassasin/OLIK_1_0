f48bfe42cc26cd7d34fb732eaf676d4eb6662cf7
lunarassasin <angrymicrowave1@gmail.com>
2025-12-20 22:01:18 +0300

    Fix package versions and main entry point

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.post('/generate-pdf', (req, res) => {
 
 app.use('/public', express.static(publicDir));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
